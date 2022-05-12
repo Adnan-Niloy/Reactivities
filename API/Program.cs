1ffbd88629269140cf07ecb897290b77e81885ed
Adnan Niloy <apache_adnan1313@hotmail.com>
2022-05-12 23:07:21 +0600

    Demo Remove and add Axios

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddDbContext<DataContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(option =>{
+    option.AddPolicy("CorsPolicy", policy =>{
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build ();
 
 // Configure the HTTP request pipeline.
@@ -24,8 +30,8 @@ if (app.Environment.IsDevelopment ()) {
 
 // app.UseHttpsRedirection();
 await SeedDatabase();
+app.UseCors("CorsPolicy");
 app.UseAuthorization ();
-
 app.MapControllers ();
 
 app.Run ();

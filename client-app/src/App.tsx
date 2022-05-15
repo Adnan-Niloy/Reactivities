24ce4e1a4ce2615cc96c9061bb99f922091c1a2f
Adnan Niloy <apache_adnan1313@hotmail.com>
2022-05-15 16:12:00 +0600

    Revert React Versions for Semantic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -14,15 +15,16 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      {/* <Header as='h2' icon='users' content='Reactivities' /> */}
-      <ul>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+
+        <List>
         {activities.map((activity: any) => (
-          <li key={activity.id}>
-            {activity.title}
-          </li>
-        ))}
-      </ul>
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
     </div>
   );
 }

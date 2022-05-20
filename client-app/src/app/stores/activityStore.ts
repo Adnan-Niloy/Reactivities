85f8fed9239a6dd7d9b146f5560d61213de8185e
Adnan Niloy <apache_adnan1313@hotmail.com>
2022-05-21 01:20:10 +0600

    Activity

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,5 +1,5 @@
 import { Activity } from './../models/activity';
-import { makeAutoObservable, runInAction } from "mobx";
+import { keys, makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 
 export default class ActivityStore {
@@ -16,6 +16,16 @@ export default class ActivityStore {
     get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) =>
             Date.parse(a.date) - Date.parse(b.date));
+    }
+
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as { [key: string]: Activity[] })
+        )
     }
 
     loadActivities = async () => {
